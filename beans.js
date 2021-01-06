f3903c0b0e3d9bd6d73bc63e13c69baef112f9ae
real-rpj11 <precy-jane.roxas@student.passerellesnumeriques.org>
2021-01-06 09:04:06 +0800

    bean counting

--- beans.js
+++ beans.js
@@ -1,6 +1,25 @@
-function countB(str) {
-    let chars = str.split('');
-    let sum = 0;
+var word = "Birthday";
+console.log("2. Bean Counting\nWORD: ", word);
 
-    console.log(chars)
+function countBs(word) {
+    var count = 0;
+    for (var i = 0; i < word.length; i++) {
+        if (word.charAt(i) == "B") {
+            count += 1;
+        }
+    }
+    return count;
 }
+console.log("Number of B: " + countBs(word));
+
+var str = "Blingbing";
+console.log("WORD: ", str);
+
+function countChar(str, char) {
+    var length1 = str.length;
+    var regexp = new RegExp(char, "g");
+    var length2 = str.replace(regexp, "").length;
+
+    return length1 - length2;
+}
+console.log("Number of i: " + countChar(str, "i"));
